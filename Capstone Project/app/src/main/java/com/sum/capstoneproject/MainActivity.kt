9742ae8719d31b9e88476b8f52bc78326d7c10df
123Sumeyra <ozugursumeyra@gmail.com>
2022-06-14 11:23:34 +0300

    half of bottom nav

--- Capstone Project/app/src/main/java/com/sum/capstoneproject/MainActivity.kt
+++ Capstone Project/app/src/main/java/com/sum/capstoneproject/MainActivity.kt
@@ -1,14 +1,49 @@
 package com.sum.capstoneproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sum.capstoneproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //val bottomNav: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val bottomNav= binding.bottomNavigationView
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        bottomNav.setupWithNavController(navController)
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+
+
+                R.id.loginFragment -> bottomNav.visibility = View.INVISIBLE
+                R.id.registerFragment -> bottomNav.visibility =View.INVISIBLE
+                R.id.forgotPasswordFragment -> bottomNav.visibility =View.INVISIBLE
+
+                else -> bottomNav.visibility =View.VISIBLE
+            }
+        }
+
+
 
     }
 }
