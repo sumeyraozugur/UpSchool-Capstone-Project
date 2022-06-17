d03b2c87dbb1ae6c0ed6a6d76994cab6d5f0bc7b
123Sumeyra <ozugursumeyra@gmail.com>
2022-06-17 11:59:31 +0300

    toolbar something done

--- Capstone Project/app/src/main/java/com/sum/capstoneproject/MainActivity.kt
+++ Capstone Project/app/src/main/java/com/sum/capstoneproject/MainActivity.kt
@@ -3,9 +3,14 @@ package com.sum.capstoneproject
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sum.capstoneproject.databinding.ActivityMainBinding
@@ -24,11 +29,26 @@ class MainActivity : AppCompatActivity() {
         val bottomNav= binding.bottomNavigationView
 
 
+
+
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
         val navController: NavController = navHostFragment.navController
 
         bottomNav.setupWithNavController(navController)
+       // setupActionBarWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.loginFragment) // bu kısımda hangi sayfalarda görünmesini istemiyorum kısmı
+        )
+        binding.toolbar.setupWithNavController(
+            navController,
+            appBarConfiguration
+        )
+
+
+
 
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
@@ -36,14 +56,23 @@ class MainActivity : AppCompatActivity() {
 
 
                 R.id.loginFragment -> bottomNav.visibility = View.INVISIBLE
-                R.id.registerFragment -> bottomNav.visibility =View.INVISIBLE
-                R.id.forgotPasswordFragment -> bottomNav.visibility =View.INVISIBLE
+                R.id.registerFragment -> {bottomNav.visibility =View.INVISIBLE}
+                R.id.forgotPasswordFragment -> {bottomNav.visibility =View.INVISIBLE}
+              
 
-                else -> bottomNav.visibility =View.VISIBLE
+                else -> {bottomNav.visibility =View.VISIBLE
+
+
+                     }
             }
         }
 
 
 
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        applicationContext
+        findNavController(R.id.fragment).popBackStack()
+        return true
     }
 }
